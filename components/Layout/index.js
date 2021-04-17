9ffc89a54f0c82a14fd93501720e0c38e3872e86
João Maranhão <joaofelipemaranhao@gmail.com>
2021-04-17 13:30:37 -0300

    style: add font preload

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -1,8 +1,14 @@
+import Head from 'next/head'
 import Navbar from '../Navbar'
 
 const Layout = ({ children }) => {
   return (
     <>
+    <Head>
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link href="https://fonts.googleapis.com/css2?family=Teko:wght@500&display=swap" rel="stylesheet" />
+      <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700;800&family=Teko:wght@500&display=swap" rel="stylesheet" />
+    </Head>
       <Navbar />
       <main>
         {children}
