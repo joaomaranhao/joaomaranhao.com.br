11c10d117f516a3b671ebd4e2f4d9619bbb644c2
João Maranhão <joaofelipemaranhao@gmail.com>
2021-04-17 13:34:53 -0300

    style: add font preload and preconnect

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -5,9 +5,11 @@ const Layout = ({ children }) => {
   return (
     <>
     <Head>
-      <link rel="preconnect" href="https://fonts.gstatic.com" />
-      <link href="https://fonts.googleapis.com/css2?family=Teko:wght@500&display=swap" rel="stylesheet" />
-      <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700;800&family=Teko:wght@500&display=swap" rel="stylesheet" />
+      <link rel="preconnect" href="https://fonts.gstatic.com"></link>
+      <link rel="preload" href="https://fonts.googleapis.com/css2?family=Teko:wght@500&display=swap" as="style"></link>
+      <link href="https://fonts.googleapis.com/css2?family=Teko:wght@500&display=swap" rel="stylesheet"></link>
+      <link rel="preload" href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700;800&family=Teko:wght@500&display=swap" as="style"></link>
+      <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700;800&family=Teko:wght@500&display=swap" rel="stylesheet"></link>
     </Head>
       <Navbar />
       <main>
