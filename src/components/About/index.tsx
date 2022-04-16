7c5a1148be4b07f998ff8aab1040648dae1b4620
João Maranhão <joaofelipemaranhao@gmail.com>
2022-04-16 00:34:24 -0300

    feat: complete About component

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -1,3 +1,7 @@
+import Image from 'next/image'
+import { SiNodedotjs, SiTypescript, SiMongodb, SiPostgresql, SiReact } from 'react-icons/si'
+
+import joao from '../../../public/images/joao.jpg'
 import styles from './About.module.css'
 
 const About = () => {
@@ -8,13 +12,37 @@ const About = () => {
           <div className={styles.text}>
             <h2>Quem sou eu?</h2>
             <p>Olá! 🖐</p>
-            <p>Meu nome é João Maranhão e eu gosto de criar coisas.</p>
-            <p>E é exatament por isso que eu adoro desenvolvimento e JavaScript!</p>
-            <p></p>
+            <p>Meu nome é João Maranhão e eu gosto de criar coisas e resolver problemas.</p>
+            <p>Formado em Comunicação Social e com mais de 10 anos de experiência em Motion Design, hoje me dedico ao desenvolvimento web.</p>
+            <p>Vejo a web como uma mídia poderosa e democrática, perfeita para compartilhar conteúdo e conhecimento.</p>
+            <p>Aqui estão algumas ferramentas que utilizo:</p>
+            <ul className={styles.tools}>
+              <li>
+                <SiNodedotjs className={`${styles.icon} ${styles.node}`} />
+              </li>
+              <li>
+                <SiTypescript className={`${styles.icon} ${styles.typescript}`} />
+              </li>
+              <li>
+                <SiReact className={`${styles.icon} ${styles.react}`} />
+              </li>
+              <li>
+                <SiMongodb className={`${styles.icon} ${styles.mongo}`} />
+              </li>
+              <li>
+                <SiPostgresql className={`${styles.icon} ${styles.postgres}`} />
+              </li>
+            </ul>
           </div>
         </div>
         <div className={styles.right}>
-
+          <div className={styles.imageContainer}>
+            <Image
+            className={styles.photo}
+            src={joao}
+            alt="Minha foto ostentando um bigodin"
+            />
+          </div>
         </div>
       </div>
 
